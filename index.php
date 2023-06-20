0e7ba20e0a0d4fc8086b23dc7253fa7e2f5c8664
Juan <78928643+jNNx@users.noreply.github.com>
2023-06-20 15:29:56 -0300

    update img

--- index.php
+++ index.php
@@ -4,15 +4,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../ejercicios.css">
-    <title>Ejercicio - 1</title>
+    <link rel="stylesheet" href="styles/index.css">
+    <title>Laboratorio II</title>
 
 </head>
 <body>
 
-        <?php include('./navbar.html');
-
-        include('./footer.html'); ?>
+        <?php include('./navbar.html');?>
+        <?php include('./footer.html'); ?>
 
 </form>
 </body>
