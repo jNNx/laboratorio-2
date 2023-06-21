92bb805961621f7f6c65a997dd3c8b4f57fcf9d0
Juan <78928643+jNNx@users.noreply.github.com>
2023-06-21 13:51:35 -0300

    last update

---
+++ php/ej21.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/ejercicios.css">
+    <title>Ejercicio - 21</title>
+</head>
+<body>
+    <?php
+        include('../navbar-2.html');?>
+        <div>
+            <img src="../img/under_construction_2018.jpg" alt="uc">
+        </div>
+        <?php include('../footer.html');
+    ?> 
+    </form>
+</body>
+</html>
